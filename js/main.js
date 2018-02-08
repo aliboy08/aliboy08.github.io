26c2e3af166904fd972baa058326b19085cd2159
aliboy08 <alistair.ponce@gmail.com>
2018-02-08 19:11:31 +0800

    Welcome update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.onreadystatechange = function(){
+
+	// Load complete
+	if( document.readyState === 'complete' ) {
+		
+		var loader = document.getElementById('loading');
+	
+		loader.style.opacity = 0;
+		
+		setTimeout(function(){
+			loader.parentNode.removeChild(loader);
+		}, 510)
+		
+	}
+
+}
